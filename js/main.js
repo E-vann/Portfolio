cb857d97064bb2e4b104b65b64f5f4d50e612b0d
Evann Le Biavant <73538387+E-vann@users.noreply.github.com>
2021-09-12 17:03:51 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// set the starting position of the cursor outside of the screen
 let clientX = 100;
 let clientY = 100;
 const innerCursor = document.querySelector(".cursor--small");
@@ -6,23 +5,12 @@ const innerCursor = document.querySelector(".cursor--small");
 var rellax = new Rellax('.rellax');
 
 const initCursor = () => {
-    // add listener to track the current mouse position
     document.addEventListener("mousemove", e => {
         clientX = e.clientX;
         clientY = e.clientY;
     });
-
-    // transform the innerCursor to the current mouse position
-    // use requestAnimationFrame() for smooth performance
     const render = () => {
         innerCursor.style.transform = `translate(${clientX}px, ${clientY}px)`;
-        // if you are already using TweenMax in your project, you might as well
-        // use TweenMax.set() instead
-        // TweenMax.set(innerCursor, {
-        //   x: clientX,
-        //   y: clientY
-        // });
-
         requestAnimationFrame(render);
     };
     requestAnimationFrame(render);
@@ -37,11 +25,6 @@ let showCursor = false;
 let group, stuckX, stuckY, fillOuterCursor;
 let radius = 20;
 
-// let link = document.getElementById(".btn");
-// link.addEventListener("mouseenter", event => {
-//     radius = 50;
-// });
-
 const initCanvas = () => {
     const canvas = document.querySelector(".cursor--canvas");
     const shapeBounds = {
@@ -53,13 +36,10 @@ const initCanvas = () => {
     const strokeWidth = 1;
     const segments = 8;
 
+    const noiseScale = 150;
+    const noiseRange = 4;
+    let isNoisy = true;
 
-    // we'll need these later for the noisy circle
-    const noiseScale = 150; // speed
-    const noiseRange = 4; // range of distortion
-    let isNoisy = true; // state
-
-    // the base shape for the noisy circle
     const polygon = new paper.Path.RegularPolygon(
         new paper.Point(0, 0),
         segments,
@@ -74,38 +54,33 @@ const initCanvas = () => {
     const noiseObjects = polygon.segments.map(() => new SimplexNoise());
     let bigCoordinates = [];
 
-    // function for linear interpolation of values
     const lerp = (a, b, n) => {
         return (1 - n) * a + n * b;
     };
 
-    // function to map a value from one range to another range
     const map = (value, in_min, in_max, out_min, out_max) => {
         return (
             ((value - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
         );
     };
-
-    // the draw loop of Paper.js 
-    // (60fps with requestAnimationFrame under the hood)
     paper.view.onFrame = event => {
-        // using linear interpolation, the circle will move 0.2 (20%)
-        // of the distance between its current position and the mouse
-        // coordinates per Frame
         lastX = lerp(lastX, clientX, 0.2);
         lastY = lerp(lastY, clientY, 0.2);
         group.position = new paper.Point(lastX, lastY);
     }
 }
 
-
 initCanvas();
-
 const magneticBtns = document.querySelectorAll('.magnetic-btn')
-
-const magneticEffect = function(e) {
-    const { offsetX: x, offsetY: y } = e
-    const { offsetWidth: width, offsetHeight: height } = this
+const magneticEffect = function (e) {
+    const {
+        offsetX: x,
+        offsetY: y
+    } = e
+    const {
+        offsetWidth: width,
+        offsetHeight: height
+    } = this
 
     const moveArea = 50
     const xMove = (x / width) * (moveArea * 2) - moveArea
